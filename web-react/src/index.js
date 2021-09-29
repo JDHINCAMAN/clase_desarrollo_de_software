07ac8b848ed270e24c4349a3532ea5b3adc69308
JD HINCAMAN <nverdolaga2003@gmail.com>
2021-09-28 20:50:28 -0500

    1era app en react terminada

--- web-react/src/index.js
+++ web-react/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
-import reactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
+import { App } from "./App";
 
-ReactDOM.render(<div>Hola Mundo</div>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
