9be3f148be92bb40710636b3df7408069bdc53ee
JD HINCAMAN <nverdolaga2003@gmail.com>
2021-10-15 11:10:27 -0500

    creados componentes

--- html/javascript/javascript.js
+++ html/javascript/javascript.js
@@ -5,12 +5,18 @@ document.addEventListener('DOMContentLoaded', function(){
     const table = document.getElementById('table');
     const alert = document.getElementById('alert');
     const btn = document.getElementById('add');
+    let id = 1;
 
     /* Ejemplo desde la terminal
     btn.onclick = function() {
         console.log('Title:', title.value);
         console.log('Description:', description.value);
     }*/
+
+    function removeTodo(id) {
+        //console.log(id);
+        document.getElementById(id).remove();
+    }
 
     function addTodo() {
         if (title.value === '' || description.value === '') {
@@ -24,6 +30,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
         alert.classList.add('d-none');
         const row = table.insertRow();
+        row.setAttribute('id', id++);
         row.innerHTML = `
             <td>${title.value}</td>
             <td>${description.value}</td>
@@ -34,11 +41,16 @@ document.addEventListener('DOMContentLoaded', function(){
                 <button class="btn btn-primary mb-1">
                   <i class="fa fa-pencil"></i>
                 </button>
-                <button class="btn btn-danger mb-1 ml-1">
-                  <i class="fa fa-trash"></i>
-                </button>
             </td>
         `;
+
+        const removeBtn = document.createElement('button');
+        removeBtn.classList.add('btn', 'btn-danger', 'mb-1', 'm1-1');
+        removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
+        removeBtn.onclick = function (e) {
+            removeTodo(row.getAttribute('id'));
+        }
+        row.children[3].appendChild(removeBtn);
     }
 
     btn.onclick = addTodo;
